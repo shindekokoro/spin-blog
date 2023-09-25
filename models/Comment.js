15daecdb0c92007ea12bf0bb26f717e33c4f4341
Brian Whisler <brian.whisler@gmail.com>
2023-09-24 18:13:47 -0600

    comments model

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const { UUIDV4, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    content: {
+      type: DataTypes.TEXT('long'),
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
