3fbd24f4c03935a707e61712d74bbb145c74443f
Brian Whisler <brian.whisler@gmail.com>
2023-09-24 18:16:26 -0600

    page titles and routing fixes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,40 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post } = require('../models');
+const isAuthed = require('../utils/auth');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [{ model: User, attributes: ['username'] }]
-    });
+    const postData = await Post.findAll({});
     const posts = postData.map((post) => post.get({ plain: true }));
-    console.log(posts);
     return res.render('homepage', {
-      posts: posts,
-      user: posts.user,
-      loggedIn: req.session.loggedIn
+      posts,
+      loggedIn: req.session.loggedIn,
+      username: req.session.username
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return res.status(500).json(err);
   }
 });
 
-// GET one post
-router.get('/post/:id', async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
-    // If the user is logged in, allow them to view the blog post
-    try {
-      const postData = await Post.findByPk(req.params.id, {});
-      const post = postData.get({ plain: true });
-      res.render('post', { post, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
+router.get('/dashboard', isAuthed, async (req, res) => {
+  try {
+    const userPostData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      }
+    });
+    const userPosts = userPostData.map((post) => post.get({ plain: true }));
+    return res.render('dashboard', {
+      title: 'Dashboard',
+      userPosts,
+      loggedIn: req.session.loggedIn,
+      username: req.session.username
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
   }
 });
 
@@ -43,14 +43,18 @@ router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     return res.redirect('/');
   }
-  return res.render('login');
+  return res.render('login', {
+    title: 'Login'
+  });
 });
 // Signup Route
 router.get('/signup', (req, res) => {
   if (req.session.loggedIn) {
     return res.redirect('/');
   }
-  return res.render('signup');
+  return res.render('signup', {
+    title: 'Signup'
+  });
 });
 
 module.exports = router;
