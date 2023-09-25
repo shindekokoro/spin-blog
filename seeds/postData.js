9b7f9d0e7e8ba12f5293db01957a1a6666e806e8
Brian Whisler <brian.whisler@gmail.com>
2023-09-24 18:12:39 -0600

    fix seed user_id

--- seeds/postData.js
+++ seeds/postData.js
@@ -5,25 +5,25 @@ const postData = [
     title: 'First Post',
     content:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus provident facilis dolorum delectus voluptate, voluptatum atque est non iusto, sint consectetur itaque cumque quis eveniet asperiores temporibus? Animi, cum fugit.',
-    author_id: 1
+    user_id: 1
   },
   {
     title: 'Second Post',
     content:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus provident facilis dolorum delectus voluptate, voluptatum atque est non iusto, sint consectetur itaque cumque quis eveniet asperiores temporibus? Animi, cum fugit.',
-    author_id: 1
+    user_id: 1
   },
   {
     title: 'Third Post',
     content:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus provident facilis dolorum delectus voluptate, voluptatum atque est non iusto, sint consectetur itaque cumque quis eveniet asperiores temporibus? Animi, cum fugit.',
-    author_id: 1
+    user_id: 1
   },
   {
     title: 'Fourth Post',
     content:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus provident facilis dolorum delectus voluptate, voluptatum atque est non iusto, sint consectetur itaque cumque quis eveniet asperiores temporibus? Animi, cum fugit.',
-    author_id: 1
+    user_id: 1
   }
 ];
 
