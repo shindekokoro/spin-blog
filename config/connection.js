7d690b60fe41c24f1fe6450c43b1e762add3ea7b
Brian Whisler <brian.whisler@gmail.com>
2023-09-24 18:11:28 -0600

    disable sql logging

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: process.env.DB_HOST,
       dialect: 'mysql',
+      logging: false,
       port: process.env.DB_PORT
     });
 
