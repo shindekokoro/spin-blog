ebd70e8c13e18f6ac4dfa057a46830a4cf03c154
Brian Whisler <brian.whisler@gmail.com>
2023-09-25 23:49:13 -0600

    add comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
+// /api/users
 const userRoutes = require('./user-routes');
 router.use('/users', userRoutes);
+
+// /api/blog (blog commenting)
+const commentRoutes = require('./blog-routes');
+router.use('/blog', commentRoutes);
 
 module.exports = router;
