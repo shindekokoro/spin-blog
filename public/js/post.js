4f6c5febd971b08b9efaa448160da910194fde99
Brian Whisler <brian.whisler@gmail.com>
2023-09-25 23:51:06 -0600

    fix wording

--- public/js/post.js
+++ public/js/post.js
@@ -14,7 +14,7 @@ const postFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert('Failed to sign up.');
+      alert('Failed to create new blog post.');
     }
   }
 };
